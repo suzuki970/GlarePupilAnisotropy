adea1d88fb9bb5e0767e7d41ad0de250dba655d1
Yuta Suzuki <suzukiy970@gmail.com>
2021-12-03 23:48:02 +0900

    MS

---
+++ [Rmd]Results/makeDataSet.R
@@ -0,0 +1,87 @@
+
+############### generate dataset file ################
+sTime = -1
+eTime = 4
+timeLen = c(sTime,eTime)
+nameOfVar = c("condition","condition")
+f1 = "Locs"
+f2 = "Pattern"
+
+g1 <- rep(c("Upper","Lower","Center","Left","Right"),2)
+g2 <- rep(c("Glare","Control"), times=c(5,5))
+
+go1 <- c("Upper","Lower","Center","Left","Right")
+go2 <- c("Glare","Control")
+
+# ################ making dataset for e1 ##################
+data=fromJSON(file="../[python]pre_processing/data/data20211124.json")
+dat <- list((matrix(unlist(data$PDR),nrow=length(data$PDR),byrow=T)),
+            t(unlist(data$sub)),
+            t(unlist(data$condition)))
+names(dat) <- c('y', 'sub', 'condition')
+
+ind_data = makePupilDataset_mat2long(dat,c('condition','condition'),timeLen,list(g1,g2),list(go1,go2),c(f1,f2))
+
+data_e1 = aggregate( . ~ sub*data_x*Locs*Pattern, data = ind_data, FUN = "mean")
+data_e1$minLatency = data$min
+
+events_data = data.frame()
+for(iSub in 1:length(data$events)){
+  events_data = rbind(events_data,
+                      data.frame(
+    sub = unique(data_e1$sub)[iSub],
+    events = unlist(data$events[iSub]),
+    data_y = unlist(data$events_p[iSub]),
+    tag = c('min',rep('ev',length(unlist(data$events[iSub]))-1))
+  ))
+}
+
+#### gaze ####
+gaze_data = data.frame(
+  sub = data$sub,
+  Locs = g1[data$condition],
+  Pattern = g2[data$condition],
+  gazeX = data$gazeX,
+  gazeY = data$gazeY
+)
+gaze_data$numOfTrial = 0
+for(iSub in unique(gaze_data$sub)){
+  gaze_data[gaze_data$sub == iSub,]$numOfTrial = 1:dim(gaze_data[gaze_data$sub == iSub,])[1]
+}
+
+#### AUC ####
+data_e1$Locs = factor(data_e1$Locs, go1)
+numOfSub = length(unique(data_e1$sub))
+
+data_auc = data.frame()
+for(iSub in unique(data_e1$sub)){
+  for(iLocs in unique(data_e1$Locs)){
+    for(iPattern in unique(data_e1$Pattern)){
+      tmp = data_e1[data_e1$sub == iSub &
+                      data_e1$Locs == iLocs &
+                      data_e1$Pattern == iPattern,]
+      tmp = tmp[tmp$data_x > tmp$minLatency[1],]
+
+      peakMin = data_e1[data_e1$sub == iSub &
+                          data_e1$Locs == iLocs &
+                          data_e1$Pattern == iPattern,]
+      peakMin =  mean(peakMin[peakMin$data_x < tmp$minLatency[1]+0.25 &
+                                peakMin$data_x > tmp$minLatency[1]-0.25,]$data_y)
+      auc = 0
+      for(ix in 1:(length(tmp$data_x))){
+        auc = auc + ((tmp[ix,]$data_y-peakMin)*(1/500))
+      }
+      data_auc = rbind(data_auc,data.frame(
+        sub = iSub,
+        Locs = iLocs,
+        Pattern = iPattern,
+        data_y = c(peakMin,auc),
+        comp = c('Early','Late')
+      ))
+    }
+  }
+}
+
+# save(data_auc,file = "./data/data_auc20211124.rda")
+save(data_e1,data_auc,
+     events_data,gaze_data,file = "./data/dataset20211124.rda")
--- [python]pre_processing/dataAnalysis.py
+++ [python]pre_processing/dataAnalysis.py
@@ -4,16 +4,20 @@ from pre_processing import pre_processing,re_sampling,moving_avg
 from rejectBlink_PCA import rejectBlink_PCA
 import json
 import os
-from pixel_size import pixel_size
+from pixel_size import pixel_size,pixel2angle
 import warnings
 import matplotlib.patches as patches
 import pandas as pd
 
 warnings.simplefilter('ignore')
 
-## ########## initial settings ###################
+#%% ###  initial settings ###################
 cfg={
 'SAMPLING_RATE':500,   
+'center':[1920, 1080],  
+'DOT_PITCH':0.369,   
+'VISUAL_DISTANCE':80,   
+'acceptMSRange':2.81,   
 'windowL':10,
 'TIME_START':-1,
 'TIME_END':4,
@@ -26,7 +30,7 @@ cfg={
 
 saveFileLocs = './data/'
 
-cfg['THRES_DIFF'] = 0.04 if cfg['METHOD'] == 1 else 0.1 ## mm
+cfg['THRES_DIFF'] = 0.1 if cfg['METHOD'] == 1 else 0.1 ## mm
 
 f = open(os.path.join(str('./data/data_original.json')))
 dat = json.load(f)
@@ -40,7 +44,7 @@ y = np.delete(y,rejectNum,axis=0)
 for mm in mmName:
     dat[mm] = [d for i,d in enumerate(dat[mm]) if not i in rejectNum]
 
-## ################## figure plot ##########################
+#%% ###  figure plot ##########################
 x = np.linspace(cfg['TIME_START'],cfg['TIME_END'],y.shape[1])
 
 plt.figure()
@@ -59,13 +63,13 @@ plt.xlim([cfg['TIME_START'],cfg['TIME_END']])
 plt.xlabel('Time from response queue')
 plt.ylabel('Changes in pupil size')
 
-#### ############### PCA ##########################
-pca_x,pca_y,rejectNumPCA = rejectBlink_PCA(y)
-y = np.delete(y,rejectNumPCA,axis=0)
-for mm in mmName:
-    dat[mm] = [d for i,d in enumerate(dat[mm]) if not i in rejectNumPCA]
+#%% ###  PCA ##########################
+# pca_x,pca_y,rejectNumPCA = rejectBlink_PCA(y)
+# y = np.delete(y,rejectNumPCA,axis=0)
+# for mm in mmName:
+#     dat[mm] = [d for i,d in enumerate(dat[mm]) if not i in rejectNumPCA]
 
-######## rejection of outlier(interplation failed trial) #########
+#%% ###  rejection of outlier(interplation failed trial) #########
 # max_val = [max(abs(y[i,])) for i in np.arange(y.shape[0])]
 # fx = np.diff(y)
 # rejectOutlier = []
@@ -83,9 +87,24 @@ for mm in mmName:
 gazeX = np.array(dat['gazeX'])
 gazeY = np.array(dat['gazeY'])
 
-################## reject gaze position ##########################
-rangeWin = pixel_size(0.369,3,80)
-center = np.array([1920, 1080])/2
+#%% ### reject gaze position ###
+rangeWin = pixel_size(cfg['DOT_PITCH'],cfg['acceptMSRange'],cfg['VISUAL_DISTANCE'])
+center = np.array(cfg['center'])/2
+
+gazeX = moving_avg(np.array(dat['gazeX']).copy(),cfg['SAMPLING_RATE'])
+gazeX = re_sampling(gazeX,(cfg['TIME_END']-cfg['TIME_START'])*100)
+
+gazeY = moving_avg(np.array(dat['gazeY']).copy(),cfg['SAMPLING_RATE'])
+gazeY = re_sampling(gazeY,(cfg['TIME_END']-cfg['TIME_START'])*100)
+
+gazeX_p = np.mean(gazeX-center[0],axis=1)
+gazeY_p = np.mean(gazeY-center[1],axis=1)
+
+gazeX_p=pixel2angle(cfg['DOT_PITCH'],gazeX_p.tolist(),cfg['VISUAL_DISTANCE'])
+gazeY_p=pixel2angle(cfg['DOT_PITCH'],gazeY_p.tolist(),cfg['VISUAL_DISTANCE'])
+
+dat['gazeX'] = gazeX_p.tolist()
+dat['gazeY'] = gazeY_p.tolist()
 
 gazeX = np.mean(gazeX-center[0],axis=1)
 gazeY = np.mean(gazeY-center[1],axis=1)
@@ -100,11 +119,12 @@ P = (tmp_x/a)+(tmp_y/b)-1
 
 rejectGaze = np.argwhere(P > 0).reshape(-1)
     
-# fig = plt.figure()
-# plt.plot(gazeX,gazeY,'.')
-# ax = plt.axes()
-# e = patches.Ellipse(xy=(0,0), width=rangeWin*2, height=rangeWin*2, fill=False, ec='r')
-# ax.add_patch(e)
+fig = plt.figure()
+ax = plt.axes()
+e = patches.Ellipse(xy=(0,0), width=rangeWin*2, height=rangeWin*2, fill=False, ec='r')
+ax.add_patch(e)
+plt.plot(gazeX,gazeY,'.')
+plt.plot(gazeX[rejectGaze],gazeY[rejectGaze],'r.')
 
 y = np.delete(y,rejectGaze,axis=0)
 for mm in mmName:
@@ -112,7 +132,8 @@ for mm in mmName:
          
 dat['PDR'] = y.tolist()
 
-################## reject subject (N < 40%) ##########################
+#%% ### participants reject ###
+
 reject=[]
 NUM_TRIAL = 150
 numOftrials = []
@@ -121,17 +142,31 @@ for iSub in np.arange(1,int(max(dat['sub']))+1):
     ind = [i for i, sub in enumerate(dat['sub']) if sub == iSub]
     numOftrials.append(len(ind))
     rejectedTrial.append(NUM_TRIAL - len(ind))
-    # if numOftrials[iSub-1] < NUM_TRIAL * 0.4:
-    #         reject.append(iSub)
+    if numOftrials[iSub-1] < NUM_TRIAL * 0.5:
+            reject.append(iSub)
             
 print('# of trials = ' + str(numOftrials))
 print('Averaged # of trials = ' + str(np.round(np.mean(numOftrials),2)))
 print('SD # of trials = ' + str(np.round(np.std(numOftrials),2)))
 
-th = NUM_TRIAL - 1.5*np.round(np.std(numOftrials),2)
-for iSub in np.arange(1,int(max(dat['sub']))+1):
-    if numOftrials[iSub-1] < th:
-        reject.append(iSub)
+
+# from scipy.stats import gamma
+# import scipy.stats as stats
+
+n, bins, patches = plt.hist(np.array(rejectedTrial))
+# a_hat, loc_hat, scale_hat = gamma.fit(n)
+# ps_hat = stats.gamma.pdf(bins, a_hat, loc=loc_hat, scale=scale_hat)
+
+# plt.figure()
+# plt.plot(ps_hat)
+
+th = np.round(np.std(rejectedTrial),2)
+# np.round(np.median(rejectedTrial),2)
+
+# for iSub in np.arange(1,int(max(dat['sub']))+1):
+#     if rejectedTrial[iSub-1] > th:
+#         reject.append(iSub)
+
 
 rejectSub = [i for i,d in enumerate(dat['sub']) if d in reject]
 print('rejected subject = ' + str(reject))
@@ -145,12 +180,11 @@ ave = np.array(rejectedTrial)/NUM_TRIAL
 print('rejected num ave = ' + str(round(np.mean(ave),3)) + ', sd = ' + str(round(np.std(ave),3)))
 
 
-########################################################################
+#%% ##### PC events #############################################
 events = {'sub':[],
           'condition':[],
           'PDR':[],
-          'min':[],
-          'ave':[]
+          'min':[]
           }
 
 for iSub in np.unique(dat['sub']):
@@ -160,18 +194,17 @@ for iSub in np.unique(dat['sub']):
                           (dat['condition'] == np.int64(iCond) )).reshape(-1)
         tmp_y = y[ind,:].mean(axis=0)
         
-       
+        tmp_y = moving_avg(tmp_y, 10).reshape(-1)
         events['sub'].append(iSub)
         events['condition'].append(iCond)
         events['PDR'].append(tmp_y.tolist())
-        
-    # events['min'].append(float(x[np.argwhere(tmp_y == np.min(tmp_y[ np.argwhere(x>0)]))]))
-
+  
 plt.figure()
-
 time_min = 0.2
 time_max = 4
 dat['min'] = []
+dat['events'] = []
+dat['events_p'] = []
 
 x_t = x[np.argwhere((x>time_min) & (x<time_max))]
 for iSub in np.unique(dat['sub']):
@@ -184,13 +217,14 @@ for iSub in np.unique(dat['sub']):
      plt.plot(x_t,tmp_p)
      
      t = tmp_p.reshape(-1)
+     
+     # second-order accurate central differences 
      pv = np.gradient(t)
     
+     # find inflection point
      indices = np.where(np.diff(np.sign(pv)))[0]
-     xv = np.gradient(indices)
-    
-     indices = indices[xv > 50]
-     
+     # plt.plot(x_t[indices],tmp_p[indices],'bo')  
+          
      ev = []
      for itr in np.arange(len(indices)):
          if pv[indices[itr]] - pv[indices[itr]+1] > 0:
@@ -200,7 +234,18 @@ for iSub in np.unique(dat['sub']):
              
      indices = indices[np.argwhere(np.array(ev) == 0).reshape(-1)]
     
+     # find inflection point
+     xv = np.diff(np.r_[0, indices])
+     
+     indices = indices[xv > 100]
+     
+     if np.diff(tmp_p[indices].reshape(-1))[0] < 0:
+         indices = indices[1:]
+     
      if len(indices)>0:
+         dat['events'].append([float(x_t[ind]) for ind in indices.tolist()])
+         dat['events_p'].append([float(tmp_p[ind]) for ind in indices.tolist()])
+         plt.plot(x_t[indices],tmp_p[indices],'bo')  
          indices = indices[0]
          plt.plot(x_t[indices],tmp_p[indices],'ro')  
          dat['min'].append(float(x_t[indices]))
@@ -210,78 +255,11 @@ for iSub in np.unique(dat['sub']):
      # ind = np.argwhere(np.array(dat['sub']) == iSub).reshape(-1)
      # tmp_y = y[ind,indices:]
      # dat['late'] = []
-        
-########################################################################
-# fs = 100
-# test_y = moving_avg(y.copy(),fs)
-# # test_y = y.copy()
-# events = {'sub':[],'condition':[],
-#           # 'indices':[],'event':[],
-#           'diff_f0':[],'diff_f1':[]}
 
-# plt.figure(figsize=(10,10))
-# for iSub in np.unique(dat['sub']):
-#     for iCond in np.unique(dat['condition']):
-        
-#         ind = np.argwhere((dat['sub'] == iSub ) &
-#                           (dat['condition'] == np.int64(iCond) )).reshape(-1)
-#         tmp_y = test_y[ind,:].mean(axis=0)
-        
-#         pv = np.gradient(tmp_y)
-    
-#         indices = np.where(np.diff(np.sign(pv)))[0]
-#         xv = np.gradient(indices)
-    
-#         indices = indices[xv > 50] # < 300ms
-        
-#         ev = []
-#         for itr in np.arange(len(indices)):
-#             if pv[indices[itr]] - pv[indices[itr]+1] > 0:
-#                 ev.append(1)
-#             else:
-#                 ev.append(0)
-#         ev = np.array(ev)
-#         ev = ev[indices > 200]
-#         indices = indices[indices > 200]
-        
-#         if len(indices) > 2:
-                  
-#             f1 = indices[ev==1]
-#             ev = ev[indices >= f1[0]]
-#             indices = indices[indices >= f1[0]]
-        
-#             if len(indices) > 2:
-#                 events['sub'].append(int(iSub))
-#                 events['condition'].append(int(iCond))
-#                 events['diff_f0'].append((tmp_y[indices[1]] - tmp_y[indices[0]]))
-#                 events['diff_f1'].append((tmp_y[indices[2]] - tmp_y[indices[1]]))
-            
-#             # events['indices'].append(indices.tolist())        
-#             # events['event'].append(ev.tolist())  
-#         # if iSub == 2:
-#             plt.subplot(5, 5,iSub)
-#             plt.plot(tmp_y)
-#             plt.plot(indices[:2],tmp_y[indices[:2]],'ro')    
-#             plt.xlim([400,2500])
-#             # plt.ylim([-0.2,0.2])
+print('The MPCL was = ' + str(round(np.mean(np.array(dat['min'])),3)) + 
+      's, sd = ' + str(round(np.std(np.array(dat['min'])),3)) + 's')
 
-# df = pd.DataFrame.from_dict(events, orient='index').T
-# name_loc = ['Upper','Lower','Center','Left','Right',
-#             'Upper','Lower','Center','Left','Right']
-# name_pat = ['Glare','Glare','Glare','Glare','Glare',
-#             'Control','Control','Control','Control','Control']
-# df['locs'] = np.array(name_loc)[np.int64(df['condition'].values-1)]
-# df['pattern'] = np.array(name_pat)[np.int64(df['condition'].values-1)]
 
-# df = df.groupby(['locs','pattern']).mean()
-# a = a.sort_values(['locs'])
-
-# df.plot.bar(y=['diff_f0'], alpha=0.6, figsize=(12,3))
-# df.plot.bar(y=['diff_f1'], alpha=0.6, figsize=(12,3))
-
-# with open(os.path.join("./data/events.json"),"w") as f:
-#     json.dump(events,f)   
-    
 ## ################## figure plot ##########################
 # plt.rcParams["font.size"] = 18
 # # 1: top glare, 2: bottom glare, 3: center glare, 4: left glare, 5: right glare; 6: top control, 7: bottom control, 8: center control, 9: left control, 10: right control.
@@ -303,8 +281,9 @@ for iSub in np.unique(dat['sub']):
                  
 # plt.legend(loc='lower right')
 
-del dat['gazeX'], dat['gazeY']
+# del dat['gazeX'], dat['gazeY']
 del dat['numOfTrial'], dat['numOfBlink'],dat['numOfSaccade'],dat['ampOfSaccade']
+
 
 with open(os.path.join("./data/data20211124.json"),"w") as f:
         json.dump(dat,f)
